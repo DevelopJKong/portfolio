ad027917afb0aa72a4ca87701180a33aeb4742df
DevelopJKong <ybaram40@gmail.com>
2021-08-21 16:53:57 +0900

    엘리클론 코딩 nav scrollintoview 완료 js

---
+++ js/mine.js
@@ -0,0 +1,6 @@
+
+const btn = document.querySelector(".home__contact");
+const contact = document.querySelector("#contact");
+btn.addEventListener("click",function(){
+    contact.scrollIntoView(true);
+});
