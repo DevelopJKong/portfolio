05e4dafccd8894f305c0986e7be805b3612fd794
DevelopJKong <ybaram40@gmail.com>
2021-08-22 12:30:48 +0900

    엘리클론 코딩 homeContact 완성

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 'use strict';
 
+
+
+
 // Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
@@ -21,6 +24,19 @@ navbarMenu.addEventListener("click",(event)=>{
     if(link === null){
         return;
     }
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior:'smooth'});
+    scrollIntoView(link);
 })
+
+
+// Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector(".home__contact");
+
+homeContactBtn.addEventListener("click",()=>{
+    scrollIntoView("#contact")
+});
+
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior:'smooth'});
+}
--- js/mine.js
+++ js/mine.js
@@ -4,3 +4,9 @@ const contact = document.querySelector("#contact");
 btn.addEventListener("click",function(){
     contact.scrollIntoView(true);
 });
+
+const homeContact = document.querySelector(".home__contact");
+//const contact = document.querySelector("#contact");
+homeContact.addEventListener("click",(e)=>{
+    contact.scrollIntoView({behavior:'smooth'});
+});
